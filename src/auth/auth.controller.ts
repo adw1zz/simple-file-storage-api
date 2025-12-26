e921b963d59c4de220bb3ad2167d2c60b50d2eb3
adw1zz <vanae089@gmail.com>
2025-12-26 17:17:46 +0300

    feature: auth-service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService, private jwtService: JwtService, private configService: ConfigService) { }
+
+    @Post('signin')
+    async signin(@Body() body: { id: string; password: string }){
+        const user = await this.authService.validateUser(body.id, body.password);
+        if (!user) {
+            return {error: ''}
+        }
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private jwtService: JwtService, private configService: ConfigService) { }
+
+    async generateTokens(payload: { id: string, device: string }) {
+        const accessToken = this.jwtService.sign(payload, {
+            secret: this.configService.get<string>('JWT_ACCESS_SECRET_KEY'),
+            expiresIn: this.configService.get<string>('JWT_ACCESS_EXPIRES_IN') as any,
+        })
+        const refreshToken = this.jwtService.sign(payload, {
+            secret: this.configService.get<string>('JWT_REFRESH_SECRET_KEY'),
+            expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRES_IN') as any,
+        })
+        return { accessToken, refreshToken };
+    }
+
+    async validateUser(id: string, password: string) {
+        if (id && password) { return { id }; } return null;
+    }
+}
