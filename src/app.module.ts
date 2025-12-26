bad1b8ce05981a440ff4e754e64f912d526738b3
adw1zz <vanae089@gmail.com>
2025-12-26 16:37:22 +0300

    chore: formatted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 
-
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+	imports: [AuthModule],
+	controllers: [],
+	providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,18 +5,22 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: false,
-      validationSchema: Joi.object({
-        JWT_ACCESS_SECRET_KEY: Joi.string().required(),
-        JWT_REFRESH_SECRET_KEY: Joi.string().required(),
-        JWT_ACCESS_EXPIRES_IN: Joi.string().pattern(/^\d+[smhd]$/).required(),
-        JWT_REFRESH_EXPIRES_IN: Joi.string().pattern(/^\d+[smhd]$/).required(),
-      })
-    }),
-  ],
-  providers: [AuthService],
-  controllers: [AuthController]
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: false,
+			validationSchema: Joi.object({
+				JWT_ACCESS_SECRET_KEY: Joi.string().required(),
+				JWT_REFRESH_SECRET_KEY: Joi.string().required(),
+				JWT_ACCESS_EXPIRES_IN: Joi.string()
+					.pattern(/^\d+[smhd]$/)
+					.required(),
+				JWT_REFRESH_EXPIRES_IN: Joi.string()
+					.pattern(/^\d+[smhd]$/)
+					.required(),
+			}),
+		}),
+	],
+	providers: [AuthService],
+	controllers: [AuthController],
 })
-export class AuthModule { }
+export class AuthModule {}
